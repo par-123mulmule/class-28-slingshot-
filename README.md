8db980714f6132ede33a8eccfb2871cbeb4cedbe
par-123mulmule <73639257+par-123mulmule@users.noreply.github.com>
2020-12-14 17:24:41 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# class-28-slingshot-
+# angryBirdsStage2.5
+Angry Birds stage 2.5 with Class Inheritance and Images
+class 26 git and github
---
+++ slingshot.js
@@ -0,0 +1,25 @@
+class Slingshot{
+    constructor(bodyA,pointB){
+    var options={
+        bodyA:bodyA,
+        pointB:pointB,
+        stiffness:0.04,
+        length:10
+    }
+    this.pointB=pointB;
+    this.sling=constraint.create(options);
+        World.add(world,this.sling)
+    }
+    dropped(){
+        this.sling.bodyA=null
+    }
+    display(){
+        if(this.sling.bodyA){
+        var pointA=this.sling.bodyA.position;
+        var pointB=this.pointB;
+        line(pointA.x,pointA.y,pointB.x,pointB.y)
+        }
+    }
+
+
+}
